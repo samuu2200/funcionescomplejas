200a06e13878c3d25e42518572ff596c22c49519
samuu2200 <samuel2200@hotmail.es>
2025-01-15 20:28:00 +0100

    Ejercicio true o false con funciones

--- src/EjecucionTextos.java
+++ src/EjecucionTextos.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class EjecucionTextos {
 
+    // Función que nos pasan un String y nos devuelve la suma
     public static void main(String[] args) {
         
         // Creamos la variable con el texto numérico para pasarselo como parametro al método
@@ -14,6 +15,21 @@ public class EjecucionTextos {
         System.out.println("La suma de "  + " es " + resultado);
 
         scan.close();
+        
+        /* ------------------------------------- */
+        System.out.println("\n ===========================\n");
+
+        //Numero ISBN 
+        String numeroISBN = "8441513929";
+
+        boolean respuesta = JuegoTextos.validarISBN(numeroISBN);
+
+        if (!respuesta) {
+            System.out.println("ISBN es INCORRECTO");
+        } else {
+            System.out.println("ISBN es CORRECTO");
+        }
+    
     }
 
 }
--- src/JuegoTextos.java
+++ src/JuegoTextos.java
@@ -1,5 +1,37 @@
 public class JuegoTextos {
 
+    public static boolean validarISBN(String numeroISBN) {
+        // Recorremos el String de números y sacamos su caracter
+        int numISBN = 0;
+        int suma = 0;
+        for (int i = 0; i < numeroISBN.length(); i++) {
+            // Recuperamos los caracteres del String
+            char charNumero = numeroISBN.charAt(i);
+
+            // Convertimo de char a String
+            String textoISBN = String.valueOf(charNumero);
+
+            // Pasamos el String a números
+            numISBN = Integer.parseInt(textoISBN);
+
+            // Multiplicamos el número ISBN por la posición que ocupa
+            int resultadoMultiplicacion = numISBN * (i + 1);
+
+            // Sumamas todos los resultados de la multiplicación
+            suma += resultadoMultiplicacion;
+
+        }
+
+        System.out.println("El resultado de la multiplicación es: " + suma);
+
+        // Dividimos el resultado entre 11 y si el resto da 0 es correcto
+        if (suma % 11 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static int getSumaTexto(String texto) {
 
         // Creamos una variable letra que va almacenar los caracteres del String
@@ -14,17 +46,18 @@ public class JuegoTextos {
             // Convertir el char a String
             String numeroString = String.valueOf(letra);
 
-            // Esta conversión primitiva primitiva recupera el valor ASCII del char que estamos recorriendocle
+            // Esta conversión primitiva primitiva recupera el valor ASCII del char que
+            // estamos recorriendocle
             int numero = Integer.parseInt(numeroString);
 
-            //Sumamaos los números
+            // Sumamaos los números
             suma += numero;
 
         }
-        
-        //Devolvemos la suma de los textos
+
+        // Devolvemos la suma de los textos
         return suma;
-        
+
     }
 
 }
